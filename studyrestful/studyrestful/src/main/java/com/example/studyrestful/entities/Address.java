d3a692441d061b37c29d1958beb9f9d81d2a0966
hangcntt <hangcntt08@gmail.com>
2019-05-30 15:55:26 +0700

    add entity

--- studyrestful/studyrestful/src/main/java/com/example/studyrestful/entities/Address.java
+++ studyrestful/studyrestful/src/main/java/com/example/studyrestful/entities/Address.java
@@ -1,4 +1,37 @@
 package com.example.studyrestful.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+
 public class Address {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "addressCategoryId", columnDefinition = "BINARY(16)", nullable = false)
+    private UUID id;
+    @Column(name = "name", columnDefinition = "NVARCHAR(255)", nullable = false)
+    private String name;
+    @Column(name = "latitude",columnDefinition = "DECIMAL(10,7)", nullable = false)
+    private String latitude;
+    @Column(name = "longtitude",columnDefinition = "DECIMAL(10,7)", nullable = false)
+    private String longtitude;
+        @Column(name = "description", columnDefinition = "NVARCHAR(1000)")
+    private String description;
+    @Column(name = "openTime", columnDefinition = "TIME")
+    private Time openedTime;
+    @Column(name = "closedTime", columnDefinition = "TIME")
+    private Time closedTime;
 }
--- studyrestful/studyrestful/src/main/java/com/example/studyrestful/entities/AddressCategory.java
+++ studyrestful/studyrestful/src/main/java/com/example/studyrestful/entities/AddressCategory.java
@@ -1,4 +1,26 @@
 package com.example.studyrestful.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "address_category")
+@Getter
+@Setter
 public class AddressCategory {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "addressCategoryId", columnDefinition = "BINARY(16)", nullable = false)
+    private UUID id;
+    @Column(name = "name", columnDefinition = "NVARCHAR(255)", nullable = false)
+    private String name;
 }
